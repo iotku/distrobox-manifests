f050b93e36396fccca6ec103c67fd4eb594ca434
Jonathan A. <me@iotku.pw>
2024-05-14 11:44:35 -0700

    Add x264-devel and various mpv/ffmpeg options for post install mpv compile

--- my-distrobox/post-install.sh
+++ my-distrobox/post-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+START_DIR="$(pwd)"
 GIT_DIR="${HOME}/git"
 if [ ! -d "$GITDIR"]; then
     mkdir "${HOME}/git"
@@ -11,7 +12,7 @@ if [ ! -d "mpv-build/" ]; then
     git clone "https://github.com/mpv-player/mpv-build.git"
 fi
 
-cd mpv-build && git pull;
+cd mpv-build && git pull; cp "$START_DIR/mpv/mpv_options" . && cp "$START_DIR/mpv/ffmpeg_options" . &&
     ./rebuild -j$(nproc) &&
     sudo ./install
 
