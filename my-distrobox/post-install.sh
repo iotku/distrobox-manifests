b39fb2fe8bc30e6e88c4758a1b692ec70aa6f841
Jonathan A. <me@iotku.pw>
2024-05-13 22:18:21 -0700

    first commit

---
+++ my-distrobox/post-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+GIT_DIR="${HOME}/git"
+if [ ! -d "$GITDIR"]; then
+    mkdir "${HOME}/git"
+fi
+
+cd "${HOME}/git"
+# Build mpd from source (more codecs)
+
+if [ ! -d "mpv-build/" ]; then
+    git clone "https://github.com/mpv-player/mpv-build.git"
+fi
+
+cd mpv-build && git pull;
+    ./rebuild -j$(nproc) &&
+    sudo ./install
+
+cd "${HOME}/git"
+
+# devour for X11 (window swallowing)
+if [ ! -d "devour/" ]; then
+    git clone "https://github.com/salman-abedin/devour.git"
+fi
+
+cd devour && git pull;
+    make &&
+    sudo make install
+
---
+++ nvbox/README.md
@@ -0,0 +1,1 @@
+Originally From: https://gist.github.com/JohnAtl/2fd83eeee94af7053b6524064577b90e
